53e1221f1ede496d1c31526dc66b57e27131dc89
Ranika23 <bveronikal@gmail.com>
2023-08-18 10:37:19 +0300

    docs: add function in index.js

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = []
+    for (let i = 0; i < expr.length; i++) {
+        if (i % 10 == 0) {
+            expr.slice(10 * (i / 10), 10 * (i / 10 + 1)) == '**********' ? arr.push(expr.slice(10 * (i / 10), 10 * (i / 10 + 1))) : arr.push(String(Number(expr.slice(10 * (i / 10), 10 * (i / 10 + 1)))))
+        }
+    }
+    result = ''
+    arrResult = []
+    resultDecode = ''
+    for (let i in arr) {
+        if (arr[i][0] == '1') {
+            for (let j = 0; j < arr[i].length; j++) {
+                if (j % 2 == 0) {
+                  arr[i].slice(j, j + 2) == '10' ? result += '.' : result += '-'
+                }
+            }
+            arrResult.push(result)
+            result = ''
+        }
+        else {arrResult.push(' ')
+            result = ''
+        }
+    }
+    for (let key in arrResult) {
+        if (arrResult[key] != ' ') {
+            resultDecode += MORSE_TABLE[arrResult[key]]
+        } 
+        else {resultDecode += ' '}
+    }
+    return resultDecode
 }
 
 module.exports = {
